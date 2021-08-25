d438041891857477fa5493d32ce4125828bb7530
Mike Drew <drewml08@gmail.com>
2021-08-25 00:35:50 -0400

    fixed a few bugs, wired up get handler for api/workouts

--- Develop/routes/api/workouts-routes.js
+++ Develop/routes/api/workouts-routes.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
+const db = require("../../models");
 
 router.get('/', (req, res) => {
-    
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.status(200).json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        })
 })
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const routes = require('./routes');
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -6,5 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
