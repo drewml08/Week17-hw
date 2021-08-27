ae0a244f6ef3d56dc088edf93f2e659edc3d31d5
Mike Drew <drewml08@gmail.com>
2021-08-27 00:26:01 -0400

    deploying

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+
+    }
+);
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
